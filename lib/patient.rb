d23567d6c41ac099f815cbbce0a1dcfbfb234aca
Micah Shute <micah.shute@gmail.com>
2018-07-06 00:23:39 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,9 +6,19 @@ class Patient
     @@all
   end
 
+  attr_accessor :doctor
+
   def initialize(name)
     @name = name
     @@all.push(self)
+  end
+
+  def appointments
+    Appointment.all.select{|apt| apt.patient == self}
+  end
+
+  def doctor 
+    Doctor.all.find{|doc| doc.patients.include?(self)}
   end
 
 end
