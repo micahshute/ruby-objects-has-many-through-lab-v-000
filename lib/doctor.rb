8435f5c11f9d41394b6c4ff7cb03add4e326d43c
Micah Shute <micah.shute@gmail.com>
2018-07-06 00:05:22 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,14 +2,15 @@ class Doctor
 
   @@all = []
 
-  def self.all 
+  def self.all
     @@all
   end
 
   attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
+    @@all << self
   end
 
 end
