c82976d0375809dc9bcd172ae8956c01f44d7451
Micah Shute <micah.shute@gmail.com>
2018-07-06 00:08:26 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :doctor, :patient, :time
+  
+  def initialize(doctor, patient, time)
+    @doctor = doctor
+    @patient = patient
+    @time = time
+  end
+end
